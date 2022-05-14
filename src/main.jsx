37e4f70511a0ae80a92949a7a318b3653de5e0fd
Nathália Elen <nathaliaelen79@gmail.com>
2022-05-14 19:25:11 -0300

    importando as bibliotecas do primereact e primeflex

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+//biblioteca de componentes
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                 //icons
+
+//cuida do display, disposição da interface
+import "primeflex/primeflex.css";
+ 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
